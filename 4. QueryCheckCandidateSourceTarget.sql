8ba8f0f8f7aec7e35872bad9a9c405aaa4bcfdd3
mahendraputra21 <mahendraputra21@gmail.com>
2023-01-18 13:43:34 +0700

    update on QueryCheckCandidateSourceTarget

--- 4. QueryCheckCandidateSourceTarget.sql
+++ 4. QueryCheckCandidateSourceTarget.sql
@@ -2,16 +2,19 @@
 ---- Query Missing Candidate CV + templates
 ---- conditional : FileExtension & CV/Doc Is NOT NULL
 ----------------------------------------------------------------------------
-SELECT 
+SELECT
+LogDate=GETDATE(),
+TemplateTypeId = ISNULL(a.TemplateTypeId,0),
 a.ApplicantId,
+a.SourceType,
+a.FileId,
 a.FolderPath,
 a.FileName,
-a.FileExtension,
-b.ApplicantId
+a.FileExtension
 FROM Needlestack.V_ApplicantDataSource a
 LEFT JOIN Needlestack.ApplicantTarget b ON a.ApplicantId = b.ApplicantId
 INNER JOIN dbo.CVContents cv ON cv.CVId = a.FileId
-WHERE b.ApplicantId IS NULL
+WHERE b.ApplicantId IS NOT NULL
 AND a.SourceType='CV'
 AND a.FileExtension IS NOT NULL
 AND cv.CV IS NOT NULL
@@ -19,15 +22,19 @@ AND cv.CV IS NOT NULL
 UNION ALL
 
 SELECT 
+LogDate=GETDATE(),
+TemplateTypeId = ISNULL(a.TemplateTypeId,0),
 a.ApplicantId,
+a.SourceType,
+a.FileId,
 a.FolderPath,
 a.FileName,
-a.FileExtension,
-b.ApplicantId
+a.FileExtension
 FROM Needlestack.V_ApplicantDataSource a
 LEFT JOIN Needlestack.ApplicantTarget b ON a.ApplicantId = b.ApplicantId
 INNER JOIN dbo.TemplateDocument t ON t.TemplateId = a.FileId
-WHERE b.ApplicantId IS NULL
+WHERE b.ApplicantId IS NOT NULL
 AND a.SourceType='T'
 AND a.FileExtension IS NOT NULL
 AND t.Document IS NOT NULL
+
